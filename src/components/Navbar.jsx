ae078eb5ad99f6eaa252e1c69fb891e326ffdd72
drashtant09 <gangwar.drashtant@gmail.com>
2023-10-07 23:21:35 +0530

    7th

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {signOut} from "firebase/auth"
 import {auth} from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
   return (
     <div className='navbar'>
         <span className='logo'>Drashtant Chat</span>
         < div className='user'>
-            <img src='https://images.pexels.com/photos/7203785/pexels-photo-7203785.jpeg?auto=compress&cs=tinysrgb&w=600' alt=''/>
-            <span>John</span>
+            <img src= {currentUser.photoURL} alt=''/>
+            <span>{currentUser.displayName}</span>
             <button onClick={()=>signOut(auth)}>logout</button>
         </div>
         </div>
